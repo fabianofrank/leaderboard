41922c135d29ff4f69441833b060ff9e3573caf4
fabianofrank <fabianofrank@protonmail.com>
2021-12-23 16:03:35 -0300

    Add API

--- src/index.js
+++ src/index.js
@@ -1,87 +1,58 @@
 /* eslint-disable prefer-template */
 import './style.css';
 
-let scores = [
-  {
-    name: 'Name: ',
-    score: 127,
-  },
-];
+const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/NVMs7bDqCFiWd9Tmg47Y/scores/';
 
-const setStored = () => localStorage.setItem('savedScore', JSON.stringify(scores));
-const getStored = () => JSON.parse(window.localStorage.getItem('savedScore'));
+// GET
+async function fetchScore() {
+  const response = await fetch(baseURL);
+  return response.json();
+};
 
-class AddScore {
-  constructor(name, score) {
-    this.name = name + ': ';
-    this.score = score;
-  }
-}
-
-// DOM
-const scoreContainer = document.querySelector('.scores');
-const formContainer = document.querySelector('.form');
-
-// Form
-const ul = document.createElement('ul');
-const liOne = document.createElement('li');
-const inputOne = document.createElement('input');
-const inputTwo = document.createElement('input');
-const liTwo = document.createElement('li');
-const button = document.createElement('button');
-button.classList.add('button');
-const submit = document.createTextNode('Submit');
-button.appendChild(submit);
-formContainer.appendChild(ul);
-ul.appendChild(liOne);
-liOne.appendChild(inputOne);
-ul.appendChild(liTwo);
-liTwo.appendChild(inputTwo);
-formContainer.appendChild(button);
-
-const iterateScore = () => {
-  scores.forEach((item, i) => {
-    scores[i].index = i;
-    const div = document.createElement('div');
-    div.classList.add('container');
-    const name = document.createElement('p');
-    const getName = document.createTextNode(item.name);
-    name.appendChild(getName);
-    const number = document.createElement('p');
-    const getScore = document.createTextNode(item.score);
-    number.appendChild(getScore);
-    div.appendChild(name);
-    div.appendChild(number);
-    scoreContainer.appendChild(div);
+// POST
+// My game ID NVMs7bDqCFiWd9Tmg47Y: console.log(response.json())
+async function postScore(objectData) {
+  const response = await fetch(baseURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(objectData), // First we create the ID for the object { name: 'Franks Game'}
   });
+  return response.json();
 };
 
-const clearPrevious = () => {
-  const oldList = document.querySelectorAll('.container');
-  [...oldList].forEach((e) => e.remove());
+// RENDER
+const formContainer = document.querySelector('.form');
+const renderScore = () => {
+  fetchScore()
+    .then((serverData) => {
+      let html = '';
+      console.log(serverData.result)
+      serverData.result.forEach((data) => {
+        let htmlSegment = `<p>${data.user}: ${data.score}</p>`;
+        html += htmlSegment;
+      });
+
+      let container = document.querySelector('.container');
+      container.innerHTML = html;
+    });
 };
 
-const displayScore = () => {
-  scores = getStored();
-  clearPrevious();
-  iterateScore();
+const getScore = () => {
+  const name = document.querySelector('.name').value;
+  const scores = document.querySelector('.scores').value;
+  console.log({ user: name, score: scores });
+  postScore({ user: name, score: scores })
+  .then(() => {
+    name.value = '';
+    scores.value = '';
+    renderScore();
+  })
 };
 
-const pushScore = () => {
-  const newScore = new AddScore(inputOne.value, inputTwo.value);
-  scores.push(newScore);
-  inputOne.value = '';
-  inputTwo.value = '';
-  setStored();
-  displayScore();
-};
+ formContainer.addEventListener('submit', (e) => {
+   getScore()
+ });
 
-const btnClass = document.querySelector('.button');
-btnClass.addEventListener('click', (e) => {
-  e.preventDefault();
-  pushScore();
-});
-
-window.addEventListener('load', () => {
-  displayScore();
-});
+renderScore();
